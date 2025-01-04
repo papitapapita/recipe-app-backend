0accae472a354fd33ccb87432bf5cf96fdd88dcb
Jonathan <many.d.r.o.2001@hotmail.com>
2025-01-04 09:24:43 -0500

    added type modules and installed dotenv for tsx

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import 'dotenv/config';
+import process from 'node:process';
 
 const app = express();
-const PORT = process.env;
+const { PORT } = process.env;
 
-app.get('/', (req: Request, res: Response) => {});
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});

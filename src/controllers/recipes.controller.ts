34997e0e7f0097e524a154b0ea3623d68da0a0ee
Jonathan <many.d.r.o.2001@hotmail.com>
2025-01-04 08:47:17 -0500

    Changes on tsconfing.json

--- src/controllers/recipes.controller.ts
+++ src/controllers/recipes.controller.ts
@@ -1,1 +1,25 @@
-export default class RecipesController {}
+import { RecipeData } from '../models/RecipeData';
+import { Request, Response } from 'express';
+import tryCatch from '../utils/tryCatch';
+import Boom from '@hapi/Boom';
+
+export default class RecipesController {
+  async getRecipes(
+    req: Request,
+    res: Response,
+    next: Function
+  ): Promise<RecipeData[]> {
+    return tryCatch(async () => {
+      let { size } = req.query;
+
+      let recipes: RecipeData[];
+
+      if (size) {
+      } else {
+        recipes = await recipesService.getAll();
+      }
+
+      return recipes;
+    });
+  }
+}

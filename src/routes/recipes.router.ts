fdb5515666d66e8a8dd79a2ab9b9145f39d0fda1
Jonathan <many.d.r.o.2001@hotmail.com>
2025-01-07 14:18:23 -0500

    Function declaration for every route

--- src/routes/recipes.router.ts
+++ src/routes/recipes.router.ts
@@ -2,4 +2,18 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  return;
+});
+
+router.get('/:id', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.patch('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+router.get('/search', (req, res) => {});

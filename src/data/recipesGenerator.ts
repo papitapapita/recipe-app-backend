fa8080fd3a52112a4325e433f0e0f834f407fe36
Jonathan <many.d.r.o.2001@hotmail.com>
2025-01-07 14:07:19 -0500

    --amend

--- src/data/recipesGenerator.ts
+++ src/data/recipesGenerator.ts
@@ -2,13 +2,25 @@ import { faker } from '@faker-js/faker';
 import { objectGenerator } from './objectGenerator';
 import { RecipeData } from '../types/RecipeData';
 import { createInstructions } from './instructionsGenerator';
+import { createTags } from './tagsGenerator';
 
 const recipesGenerator = objectGenerator<RecipeData>(
   (index?) => ({
     recipeId: index!,
     title: faker.food.dish(),
+    description: faker.food.description(),
     imageUrl: faker.image.url(),
+    preparingTime: faker.number.int(180),
+    cookingTime: faker.number.int(180),
+    calories: faker.number.int(1000),
+    carbs: faker.number.int(1000),
+    protein: faker.number.int(1000),
+    fat: faker.number.int(1000),
     instructions: createInstructions(5),
+    tags: createTags(3),
+    createdAt: Date().toString(),
     updatedAt: Date().toString()
   })
 );
+
+export { recipesGenerator };

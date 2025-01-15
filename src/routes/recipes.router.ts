27424b55e2568f07b31e88fba5e1ea421bb95ad1
Jonathan <many.d.r.o.2001@hotmail.com>
2025-01-15 10:27:29 -0500

    Recipes routes implemented

--- src/routes/recipes.router.ts
+++ src/routes/recipes.router.ts
@@ -1,19 +1,44 @@
 import express from 'express';
+import validate from '../middlewares/validator';
+import { idSchema, recipeSchema } from '../utils/schemas';
+import RecipeController from '../controllers/recipes.controller';
+
+const {
+  getRecipe,
+  getRecipes,
+  createRecipes,
+  updateRecipe,
+  replaceRecipe,
+  deleteRecipe
+} = new RecipeController();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return;
-});
+router.get('/', getRecipes());
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', validate(idSchema, 'params'), getRecipe());
 
-router.post('/', (req, res) => {});
+router.post('/', validate(recipeSchema, 'body'), createRecipes());
 
-router.put('/:id', (req, res) => {});
+router.put(
+  '/:id',
+  validate(idSchema, 'params'),
+  validate(recipeSchema, 'body'),
+  replaceRecipe()
+);
 
-router.patch('/:id', (req, res) => {});
+router.patch(
+  '/:id',
+  validate(idSchema, 'params'),
+  validate(recipeSchema, 'body'),
+  updateRecipe()
+);
 
-router.delete('/:id', (req, res) => {});
+router.delete(
+  '/:id',
+  validate(idSchema, 'params'),
+  validate(recipeSchema, 'body'),
+  deleteRecipe()
+);
 
-router.get('/search', (req, res) => {});
+//router.get('/search', validate);
